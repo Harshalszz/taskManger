e3162f0e943116ad293a06f59b9462d1c32b6428
Harshalszz <477harshz@gmail.com>
2025-07-17 20:06:15 +0530

    add to enpoints of delete the task by id , and other one of setting the isComplete flag

--- src/main/java/com/project/taskManager/controller/TaskController.java
+++ src/main/java/com/project/taskManager/controller/TaskController.java
@@ -5,10 +5,7 @@ import com.project.taskManager.service.TaskService;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +30,18 @@ public class TaskController {
     @PostMapping
     public String createTask(@RequestParam String title){
         taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/isFinished")
+    public String isFinished(@PathVariable Long id){
+        taskService.isFinished(id);
         return "redirect:/";
     }
 }
--- src/main/java/com/project/taskManager/service/TaskService.java
+++ src/main/java/com/project/taskManager/service/TaskService.java
@@ -28,4 +28,17 @@ public class TaskService {
         taskRepository.save(task);
 
     }
+
+    public void deleteTask(Long id) {
+
+        taskRepository.deleteById(id);
+    }
+
+    public void isFinished(Long id) {
+
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid task id"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+    }
 }
